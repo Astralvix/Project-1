abf7379d5b60005f0fe572e3d766398028d8d648
Erik Haklar <erikhaklar@gmail.com>
2019-10-24 21:48:47 -0500

    started Junit testing

--- ServletP1/src/test/java/com/revature/test/ServletTest.java
+++ ServletP1/src/test/java/com/revature/test/ServletTest.java
@@ -2,13 +2,26 @@ package com.revature.test;
 
 import static org.junit.Assert.*;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+
+import com.revature.dao.ERFormDaoImpl;
+import com.revature.pojo.ERForm;
+
 import org.junit.*;
 public class ServletTest {
+	
+	public static final ERFormDaoImpl formDaoImpl = new ERFormDaoImpl();
+	
+	private List<ERForm> ERFormList = new ArrayList<ERForm>();
+	private LocalDate testDate; 
 
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
@@ -27,8 +40,9 @@ public class ServletTest {
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void getERFormsByErik() {
+		ERFormList.add(new ERForm(0, "test", "test", date, , null, null, 0, null, null, null, null, null, null));
+		assertEquals(ERFormList, formDaoImpl.getMyForms("testUser"));
 	}
 
 }
