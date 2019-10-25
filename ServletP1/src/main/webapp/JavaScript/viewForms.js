fc7e099528979152b505ba53da5883d02aad0091
Erik Haklar <erikhaklar@gmail.com>
2019-10-24 19:31:26 -0500

    added js for requests and status

--- ServletP1/src/main/webapp/JavaScript/viewForms.js
+++ ServletP1/src/main/webapp/JavaScript/viewForms.js
@@ -19,6 +19,48 @@ class ReimbursementPojo {
     }
 }
 
+class RequestsPojo {
+    constructor(msg_id, sender_user, rec_user, messages, date_sent) {
+        this.msg_id = msg_id;
+        this.sender_user = sender_user;
+        this.rec_user = rec_user;
+        this.messages = messages;
+        this.date_sent = date_sent;
+    }
+
+}
+
+class FormsStatusPojo {
+    constructor(RID_status, manager_status, depthead_status, benco_status) {
+        this.RID_status = RID_status;
+        this.manager_status = manager_status = manager_status;
+        this.depthead_status = depthead_status;
+        this.benco_status = benco_status;
+    }
+}
+
+function createRequestsList(requests) {
+    let theTable = document.getElementById("table");
+
+    for (r of requests) {
+
+        let row = theTable.insertRow(1);
+
+        
+        let cell0 = row.insertCell(0);
+        let cell1 = row.insertCell(1);
+        let cell2 = row.insertCell(2);
+        let cell3 = row.insertCell(3);
+        let cell4 = row.insertCell(4);
+        
+        cell0.innerHTML = r.msg_id;
+        cell1.innerHTML = r.sender_user;
+        cell2.innerHTML = r.rec_user;
+        cell3.innerHTML = r.messages;
+        cell4.innerHTML = r.date_sent; 
+    }
+}
+
 //let testf = new ReimbursementPojo("RID", "userName", "fullName", "theDate", "eventStartDate", "theLocation", "description", "theCost", "gradingFormat", "passingPercentage", "eventType", "reason", "fileName", "status");
 function createFormsList(forms) {
 
@@ -85,7 +127,7 @@ function createFormsList(forms) {
 
 function getOwnedForms() {
 
-    console.log("I was called");
+    //console.log("I was called");
 
     let xhr = new XMLHttpRequest();
 
@@ -97,7 +139,7 @@ function getOwnedForms() {
                 createFormsList(JSON.parse(xhr.responseText));
                 document.getElementById("retrievalInfo").innerHTML = "Reimbursement forms recieved!";
             } else {
-                document.getElementById("retrievalInfo").innerHTML = "Failed to retireve forms";
+                document.getElementById("retrievalInfo").innerHTML = "Failed to retrieve forms";
             }
         } else {
             document.getElementById("retrievalInfo").innerHTML = "Fetching Request";
@@ -112,7 +154,7 @@ function getOwnedForms() {
 
 function getAllForms() {
 
-    console.log("I was called");
+    //console.log("I was called");
 
     let xhr = new XMLHttpRequest();
 
@@ -124,7 +166,7 @@ function getAllForms() {
                 createFormsList(JSON.parse(xhr.responseText));
                 document.getElementById("retrievalInfo").innerHTML = "Reimbursement forms recieved!";
             } else {
-                document.getElementById("retrievalInfo").innerHTML = "Failed to retireve forms";
+                document.getElementById("retrievalInfo").innerHTML = "Failed to retrieve forms";
             }
         } else {
             document.getElementById("retrievalInfo").innerHTML = "Fetching Request";
@@ -132,7 +174,79 @@ function getAllForms() {
     }
     xhr.open("GET", "ViewAllForms", true);
     xhr.send();
+}
 
-    //Redirect
-    //window.location = "http://localhost:8080/viewOwnedForms.html";
+
+function getRequests() {
+
+    //console.log("I was called");
+
+    let xhr = new XMLHttpRequest();
+
+
+    //this will only run after open is called and the ready state is changed
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4) {
+            if (xhr.status === 200) {
+                createRequestsList(JSON.parse(xhr.responseText));
+                document.getElementById("retrievalInfo").innerHTML = "Requests recieved!";
+            } else {
+                document.getElementById("retrievalInfo").innerHTML = "Failed to retrieve requests";
+            }
+        } else {
+            document.getElementById("retrievalInfo").innerHTML = "Fetching Request";
+        }
+    }
+    xhr.open("GET", "ViewRequests", true);
+    xhr.send();
+}
+
+
+function getAllFormsStatus() {
+
+    //console.log("I was called");
+
+    let xhr = new XMLHttpRequest();
+
+
+    //this will only run after open is called and the ready state is changed
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4) {
+            if (xhr.status === 200) {
+                createAllStatusList(JSON.parse(xhr.responseText));
+                document.getElementById("retrievalInfo").innerHTML = "Forms status recieved!";
+            } else {
+                document.getElementById("retrievalInfo").innerHTML = "Failed to retrieve forms status";
+            }
+        } else {
+            document.getElementById("retrievalInfo").innerHTML = "Fetching Request";
+        }
+    }
+    xhr.open("GET", "ViewAllStatus", true);
+    xhr.send();
+}
+
+
+function getMyFormsStatus() {
+
+    //console.log("I was called");
+
+    let xhr = new XMLHttpRequest();
+
+
+    //this will only run after open is called and the ready state is changed
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4) {
+            if (xhr.status === 200) {
+                createMyStatusList(JSON.parse(xhr.responseText));
+                document.getElementById("retrievalInfo").innerHTML = "Forms status recieved!";
+            } else {
+                document.getElementById("retrievalInfo").innerHTML = "Failed to retrieve forms status";
+            }
+        } else {
+            document.getElementById("retrievalInfo").innerHTML = "Fetching Request";
+        }
+    }
+    xhr.open("GET", "ViewStatus", true);
+    xhr.send();
 }
